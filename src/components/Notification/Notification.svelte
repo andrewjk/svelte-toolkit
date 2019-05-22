e4aeb5dbe5cc6b46e86690d26652ce1fa1d67d14
Andrew K <andrewjk@gmail.com>
2019-05-22 19:05:13 +1000

    Implemented notifications

--- src/components/Notification/Notification.svelte
+++ src/components/Notification/Notification.svelte
@@ -1,25 +1,56 @@
 <script>
+  import { onMount, createEventDispatcher } from "svelte";
+  import { fade } from "svelte/transition";
+
   export let id = null;
   export let className = null;
-  export let type = "info";
+  export let type = "default";
   export let message = "";
+  export let closable = true;
+  export let duration = -1;
+
+  const dispatch = createEventDispatcher();
+
+  let visible = true;
+
+  onMount(() => {
+    if (duration > 0) {
+      setTimeout(close, duration);
+    }
+  });
+
+  function close() {
+    visible = false;
+    dispatch('closed');
+  }
 </script>
 
 <style>
-  .notification.info {
-    color: blue;
+  .notification {
+    display: inline-flex;
   }
-  .notification.success {
-    color: limegreen;
+
+  .notification-message {
+    flex: 1 1 auto;
   }
-  .notification.warning {
-    color: orange;
-  }
-  .notification.danger {
-    color: red;
+
+  .notification-close {
+    flex: 0 0 auto;
   }
 </style>
 
-<div {id} class={['notification', type, className].filter(Boolean).join(' ')}>
-  <slot>{message}</slot>
-</div>
+{#if visible}
+  <div
+    {id}
+    class={['notification', type, className].filter(Boolean).join(' ')}
+    out:fade>
+    <div class="notification-message">
+      <slot>{message}</slot>
+    </div>
+    {#if closable}
+      <div class="notification-close">
+        <button class="close-button" on:click={close} />
+      </div>
+    {/if}
+  </div>
+{/if}
