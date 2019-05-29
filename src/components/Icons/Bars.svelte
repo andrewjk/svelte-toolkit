786c7192018d10abc1a310e076feb6cdb1a76015
Andrew K <andrewjk@gmail.com>
2019-05-29 16:48:37 +1000

    Added a Bars icon

---
+++ src/components/Icons/Bars.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let id = null;
+  export let className = null;
+</script>
+
+<svg
+  {id}
+  class={['icon', className].filter(Boolean).join(' ')}
+  viewBox="0 0 100 100">
+  <g>
+    <path d="M15,25 L85,25" />
+    <path d="M15,50 L85,50" />
+    <path d="M15,75 L85,75" />
+  </g>
+</svg>
