bcd9984d4c265780d9767315a89529a7e8469206
Andrew K <andrewjk@gmail.com>
2019-06-26 12:55:31 +1000

    Add autofocus manually

--- src/forms/TextBox/TextBox.svelte
+++ src/forms/TextBox/TextBox.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { getContext, createEventDispatcher } from "svelte";
+  import { onMount, getContext, createEventDispatcher } from "svelte";
 
   export let id = null;
   export let name = null;
@@ -9,14 +9,25 @@
 
   export let type = "";
   export let value = "";
+  export let autofocus = false;
 
   export let validator = null;
   export let maxlength = 0;
 
+  let input = null;
   let originalType = type;
   let setValue = null;
 
   const dispatch = createEventDispatcher();
+
+  onMount(() => {
+    // We're manually focussing in onMount for two reasons:
+    // 1. to avoid an overly-broad accessibility warning (there are instances where autofocus is desirable)
+    // 2. to enable autofocus when an item is dynamically added to a page (e.g. adding a new item at the bottom of a list)
+    if (autofocus) {
+      input.focus();
+    }
+  });
 
   // Register this item with the parent Field (if applicable), which will let us know when we are invalid
   const context = getContext("field");
