316190dc18082959c0c68922259b4f79af1d54ab
Andrew K <andrewjk@gmail.com>
2019-07-02 13:34:41 +1000

    Added total property to progress

--- src/components/Progress/Progress.svelte
+++ src/components/Progress/Progress.svelte
@@ -5,8 +5,11 @@
   export let type = "info";
   export let size = "";
   export let value = 0;
+  export let total = 100;
+
+  $: percent = Math.min(value / total * 100, 100)
 </script>
 
 <div {id} class={['progress', type, size, className].filter(Boolean).join(' ')}>
-  <div class="progress-bar" style={`width: ${value}%`}/>
+  <div class="progress-bar" style={`width: ${percent}%`}/>
 </div>
