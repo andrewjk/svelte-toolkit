a67f20c316aae05a591a1029bc90495f02d9dd96
Andrew K <andrewjk@gmail.com>
2019-08-16 15:47:02 +1000

    Support a table with either an array of items, or data loaded externally

--- src/components/Table/Table.svelte
+++ src/components/Table/Table.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { setContext, createEventDispatcher } from "svelte";
+  import { setContext, onMount, createEventDispatcher } from "svelte";
 
   import TableHeader from "./TableHeader.svelte";
   import Pagination from "../Pagination/Pagination.svelte";
@@ -9,11 +9,19 @@
   export { className as class };
   export let classNames = [];
 
-  export let data = [];
-  export let keyField = "";
+  // Pass in items if you want the table to handle paging, sorting, etc, or data if you want to handle it (e.g. by fetching from a web service)
+  export let items = null;
+  export let data = null;
+  export let keyField;
   export let columns = [];
 
-  let view = null;
+  export let pageNumber = 1;
+  export let pageSize = 10;
+  export let itemCount = 0;
+
+  export let type = "";
+
+  let view = buildView();
 
   const dispatch = createEventDispatcher();
 
@@ -41,10 +49,12 @@
     for (let i = 0; i < columns.length; i++) {
       if (columns[i].field === field) {
         if (columns[i].sortDirection === "asc") {
+          pageNumber = 1;
           columns[i].sortDirection = "desc";
           columns[i].setSortDirection("desc");
           dispatch("sort", { sort: field, sortDirection: "desc" });
         } else {
+          pageNumber = 1;
           columns[i].sortDirection = "asc";
           columns[i].setSortDirection("asc");
           dispatch("sort", { sort: field, sortDirection: "asc" });
@@ -54,13 +64,56 @@
         columns[i].setSortDirection("");
       }
     }
+    view = buildView();
+  }
+
+  function handlePage(e) {
+    pageNumber = e.detail;
+    view = buildView();
+    dispatch("page", pageNumber);
+  }
+
+  function buildView() {
+    if (items) {
+      itemCount = items.length;
+
+      let newView = items.slice(0, items.length);
+
+      // Sort the items
+      for (let i = 0; i < columns.length; i++) {
+        if (columns[i].sortDirection) {
+          newView.sort(
+            compare(columns[i].field, columns[i].sortDirection === "desc")
+          );
+        }
+      }
+
+      // Page the items
+      const start = (pageNumber - 1) * pageSize;
+      const end = start + pageSize;
+      newView = newView.slice(start, end);
+
+      return newView;
+    } else if (data) {
+      // The user should have passed us the sorted, paged data
+      return data;
+    }
+  }
+
+  // From https://stackoverflow.com/a/979325
+  function compare(field, reverse, primer) {
+    var key = primer ? x => primer(x[field]) : x => x[field];
+    reverse = !reverse ? 1 : -1;
+    return function(a, b) {
+      return (a = key(a)), (b = key(b)), reverse * ((a > b) - (b > a));
+    };
   }
 </script>
 
-{#if data && data.length}
+{#if view && view.length}
   <table
     {id}
-    class={['table', className]
+    class={['table', type, className]
       .concat(classNames)
       .filter(Boolean)
       .join(' ')}>
@@ -79,7 +132,7 @@
       </slot>
     </thead>
     <tbody>
-      {#each data as item (item[keyField])}
+      {#each view as item (item[keyField])}
         <tr>
           <slot {item} />
         </tr>
@@ -89,3 +142,4 @@
 {:else}
   <slot name="empty" />
 {/if}
+<Pagination {pageNumber} {pageSize} {itemCount} on:change={handlePage} />
