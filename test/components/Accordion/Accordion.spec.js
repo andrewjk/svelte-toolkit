1dec28210075e87ad61146bc6c850ae75f58c3e7
Andrew K <andrewjk@gmail.com>
2020-01-13 16:49:24 +1100

    Test: slightly more in-depth Accordion testing

---
+++ test/components/Accordion/Accordion.spec.js
@@ -0,0 +1,33 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, waitForElementToBeRemoved } from '@testing-library/svelte';
+
+import AccordionSpec from './AccordionSpec.svelte';
+
+test('accordion creation', async () => {
+    const { container, getByText } = render(Accordion, { id: 'accordion' });
+
+    expect(container.querySelector('#accordion')).toBeInTheDocument();
+});
+
+test('accordion expanding', async () => {
+    const { container, getByText, queryByText } = render(AccordionSpec);
+
+    expect(queryByText('Body 1')).not.toBeInTheDocument();
+    expect(queryByText('Body 2')).not.toBeInTheDocument();
+    expect(queryByText('Body 3')).not.toBeInTheDocument();
+
+    // Clicking header 1 should reveal body 1
+    await fireEvent.click(getByText('Header 1'));
+    expect(queryByText('Body 1')).toBeInTheDocument();
+
+    // Clicking header 2 should hide body 1 and reveal body 2
+    await fireEvent.click(getByText('Header 2'));
+    await waitForElementToBeRemoved(() => queryByText('Body 1'))
+    expect(queryByText('Body 1')).not.toBeInTheDocument();
+    expect(queryByText('Body 2')).toBeInTheDocument();
+
+    // Clicking header 2 again should hide body 2
+    await fireEvent.click(getByText('Header 2'));
+    await waitForElementToBeRemoved(() => queryByText('Body 2'))
+    expect(queryByText('Body 2')).not.toBeInTheDocument();
+});
---
+++ test/components/Accordion/AccordionSpec.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Accordion from "../../../src/components/Accordion/Accordion.svelte";
+  import AccordionItem from "../../../src/components/Accordion/AccordionItem.svelte";
+</script>
+
+<Accordion id="accordion">
+  <AccordionItem id="item-1" header="Header 1">Body 1</AccordionItem>
+  <AccordionItem id="item-2" header="Header 2">Body 2</AccordionItem>
+  <AccordionItem id="item-3" header="Header 3">Body 3</AccordionItem>
+</Accordion>
