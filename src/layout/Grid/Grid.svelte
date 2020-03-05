5ad527d813ce7ddfe72ec8f3108e63e5303784ac
Andrew K <andrewjk@gmail.com>
2020-03-05 17:05:50 +1100

    Breaking: change Grid and GridItem to use CSS grid

--- src/layout/Grid/Grid.svelte
+++ src/layout/Grid/Grid.svelte
@@ -5,20 +5,33 @@
   let className = null;
   export { className as class };
 
-  export let spaced = null;
-  export let nowrap = null;
+  export let cols = null;
+  export let smcols = null;
+  export let mdcols = null;
+  export let lgcols = null;
+  export let xlcols = null;
 
-  setContext("grid", {
-    spaced
-  });
+  export let spaced = null;
 </script>
 
-<style src="Grid.scss">
+<!-- HACK: global because we're putting classes together with strings -->
+<style src="Grid.scss" global>
 
 </style>
 
 <div
   {id}
-  class={['grid', spaced ? 'spaced' : null, nowrap ? 'nowrap' : null, className].filter(Boolean).join(' ')}>
+  class={[
+      'grid',
+      spaced ? 'spaced' : null,
+      cols ? 'cols-' + cols : null,
+      smcols ? 'sm-cols-' + smcols : null,
+      mdcols ? 'md-cols-' + mdcols : null,
+      lgcols ? 'lg-cols-' + lgcols : null,
+      xlcols ? 'xl-cols-' + xlcols : null,
+      className
+    ]
+    .filter(Boolean)
+    .join(' ')}>
   <slot />
 </div>
--- src/layout/Grid/GridItem.svelte
+++ src/layout/Grid/GridItem.svelte
@@ -5,30 +5,18 @@
   let className = null;
   export { className as class };
 
-  export let width = null;
-  export let smwidth = null;
-  export let mdwidth = null;
-  export let lgwidth = null;
-  export let xlwidth = null;
+  export let span = null;
+  export let smspan = null;
+  export let mdspan = null;
+  export let lgspan = null;
+  export let xlspan = null;
 
-  export let order = null;
-  export let smorder = null;
-  export let mdorder = null;
-  export let lgorder = null;
-  export let xlorder = null;
-
-  export let nowrap = false;
-  export let smnowrap = false;
-  export let mdnowrap = false;
-  export let lgnowrap = false;
-  export let xlnowrap = false;
-
-  export let spaced = null;
-
-  const context = getContext("grid");
-  if (context) {
-    spaced = context.spaced;
-  }
+  // TODO: Order doesn't work in CSS grid
+  //export let order = null;
+  //export let smorder = null;
+  //export let mdorder = null;
+  //export let lgorder = null;
+  //export let xlorder = null;
 </script>
 
 <!-- HACK: global because we're putting classes together with strings -->
@@ -38,25 +26,18 @@
 
 <div
   {id}
-  class={
-    [
+  class={[
       'grid-item',
-      width ? 'width-' + width : null,
-      smwidth ? 'sm-width-' + smwidth : null,
-      mdwidth ? 'md-width-' + mdwidth : null,
-      lgwidth ? 'lg-width-' + lgwidth : null,
-      xlwidth ? 'xl-width-' + xlwidth : null,
-      order ? 'order-' + order : null,
-      smorder ? 'sm-order-' + smorder : null,
-      mdorder ? 'md-order-' + mdorder : null,
-      lgorder ? 'lg-order-' + lgorder : null,
-      xlorder ? 'xl-order-' + xlorder : null,
-      nowrap ? 'nowrap' : null,
-      smnowrap ? 'sm-nowrap' : null,
-      mdnowrap ? 'md-nowrap' : null,
-      lgnowrap ? 'lg-nowrap' : null,
-      xlnowrap ? 'xl-nowrap' : null,
-      spaced ? 'spaced' : null,
+      span ? 'span-' + span : null,
+      smspan ? 'sm-span-' + smspan : null,
+      mdspan ? 'md-span-' + mdspan : null,
+      lgspan ? 'lg-span-' + lgspan : null,
+      xlspan ? 'xl-span-' + xlspan : null,
+      //order ? 'order-' + order : null,
+      //smorder ? 'sm-order-' + smorder : null,
+      //mdorder ? 'md-order-' + mdorder : null,
+      //lgorder ? 'lg-order-' + lgorder : null,
+      //xlorder ? 'xl-order-' + xlorder : null,
       className
     ]
     .filter(Boolean)
