475b405d21c2e7cd81d219d23479455c43dca6f3
Andrew K <andrewjk@gmail.com>
2021-06-01 16:18:38 +1000

    Feat: add a change event to DropDownMenu

--- src/components/DropDownMenu/DropDownMenu.svelte
+++ src/components/DropDownMenu/DropDownMenu.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { getContext, setContext, tick } from "svelte";
+  import { getContext, setContext, createEventDispatcher } from "svelte";
 
   export let id = null;
   let className = null;
@@ -7,18 +7,31 @@
 
   export let position = null;
 
+  const dispatch = createEventDispatcher();
+
+  // Get the horizontal position from the context (e.g. if this menu is used
+  // in a NavMenuDropDown at the end of the NavMenu it will inherit the "end"
+  // position)
   var context = getContext("dropdownmenu");
   if (context) {
     position = context.position;
   }
+
+  // Pass the handleClick method to DropDownMenuItems so that we can pass along
+  // a change method
+  setContext("dropdownmenuitem", {
+    handleClick: () => {
+      dispatch("change");
+    },
+  });
 </script>
-
-<style src="DropDownMenu.scss">
-
-</style>
 
 <div
   {id}
-  class={['drop-down-menu', position, className].filter(Boolean).join(' ')}>
+  class={["drop-down-menu", position, className].filter(Boolean).join(" ")}
+>
   <slot />
 </div>
+
+<style src="DropDownMenu.scss">
+</style>
--- src/components/DropDownMenu/DropDownMenuItem.svelte
+++ src/components/DropDownMenu/DropDownMenuItem.svelte
@@ -1,18 +1,30 @@
 <script>
+  import { getContext } from "svelte";
+
   export let id = null;
   let className = null;
   export { className as class };
   export let active = false;
+
+  let handleClick;
+
+  // If we received a context with a handleClick function in it (from the
+  // parent DropDownMenu), we call it on click
+  var context = getContext("dropdownmenuitem");
+  if (context) {
+    handleClick = context.handleClick;
+  }
 </script>
-
-<style src="DropDownMenuItem.scss">
-
-</style>
 
 <div
   {id}
-  class={['drop-down-item', className].filter(Boolean).join(' ')}
+  class={["drop-down-item", className].filter(Boolean).join(" ")}
   class:active
-  role="menuitem">
+  role="menuitem"
+  on:click={() => (handleClick ? handleClick() : null)}
+>
   <slot />
 </div>
+
+<style src="DropDownMenuItem.scss">
+</style>
