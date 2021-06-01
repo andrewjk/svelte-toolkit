07d8e7fe9feedc070d0ebca5059c01ef7d1a43cb
Andrew K <andrewjk@gmail.com>
2021-06-01 16:17:30 +1000

    Fix: make NavMenuDropDown behave more like the standard DropDowns (close when click off or click item, etc)

--- src/navigation/NavBar/NavMenuDropDown.svelte
+++ src/navigation/NavBar/NavMenuDropDown.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { getContext, setContext } from "svelte";
+  import { getContext, setContext, onDestroy } from "svelte";
 
   import ChevronDown from "../../icons/ChevronDown";
   import DropDownMenu from "../../components/DropDownMenu/DropDownMenu";
@@ -8,76 +8,97 @@
   let className = null;
   export { className as class };
   export let active = false;
+  export let expanded = false;
 
-  let expanded = false;
+  $: expandedChanged(expanded);
 
-  // HACK: To avoid an accessibility warning - but I think it's fine to use an anchor with href="#" on a drop-down?
+  let container;
+
+  // HACK: To avoid an accessibility warning - but I think it's fine to use an
+  // anchor with href="#" on a drop-down?
   const href = "#";
 
   var context = getContext("navmenu");
   if (context) {
     setContext("dropdownmenu", {
-      position: context.position
+      position: context.position,
     });
   }
 
-  function showDropDown() {
-    expanded = true;
+  onDestroy(() => {
+    if (expanded) {
+      document.removeEventListener("click", handleCloseClick);
+    }
+  });
+
+  function expandedChanged(value) {
+    showOrHide();
   }
 
-  function hideDropDown() {
-    expanded = false;
-  }
-
-  function toggleDropDown() {
+  function toggleDropDown(e) {
+    e.preventDefault();
     expanded = !expanded;
   }
 
+  function showOrHide() {
+    if (typeof window !== "undefined") {
+      if (expanded) {
+        document.addEventListener("click", handleCloseClick);
+      } else {
+        document.removeEventListener("click", handleCloseClick);
+      }
+    }
+  }
+
+  function handleCloseClick(e) {
+    if (!container.contains(e.target)) {
+      toggleDropDown(e);
+    }
+  }
+
   function handleMouseEnter(e) {
-    showDropDown();
+    if (!expanded) {
+      toggleDropDown(e);
+    }
   }
 
   function handleMouseLeave(e) {
-    hideDropDown();
+    if (expanded) {
+      toggleDropDown(e);
+    }
   }
 
-  function handleMouseDown(e) {
-    toggleDropDown();
-  }
-
-  function handleTouchStart(e) {
-    // NOTE: These seem both to be required to prevent the mouse events from being fired on touch screens:
-    e.preventDefault();
-    e.stopPropagation();
-    toggleDropDown();
+  function handleClick(e) {
+    toggleDropDown(e);
   }
 </script>
 
-<style src="NavMenuDropDown.scss">
-
-</style>
-
 <li
   {id}
-  class={['nav-menu-drop-down', className].filter(Boolean).join(' ')}
+  class={["nav-menu-drop-down", className].filter(Boolean).join(" ")}
   class:active
   role="menuitem"
+  bind:this={container}
   on:mouseenter={handleMouseEnter}
-  on:mouseleave={handleMouseLeave}>
+  on:mouseleave={handleMouseLeave}
+>
   <a
     {href}
     class="nav-menu-link"
     aria-haspopup="true"
-    on:mousedown={handleMouseDown}
-    on:touchstart={handleTouchStart}>
+    on:click={handleClick}
+  >
     <slot />
     <slot name="button">
       <ChevronDown />
     </slot>
   </a>
   {#if expanded}
-    <DropDownMenu>
+    <DropDownMenu on:change={() => expanded = false}>
       <slot name="menu" />
     </DropDownMenu>
   {/if}
 </li>
+
+<style src="NavMenuDropDown.scss">
+</style>
