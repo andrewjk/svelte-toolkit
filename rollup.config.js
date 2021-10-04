b23fce3de0c2d8a3dd5b7e7f363ba0f6bfdf4700
Andrew K <andrewjk@gmail.com>
2021-10-04 15:59:48 +1100

    Chore: get rollup to build scss
    Did something change? I swear this used to just work...

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,29 @@
-import svelte from 'rollup-plugin-svelte';
-import autoPreprocess from 'svelte-preprocess';
-import resolve from 'rollup-plugin-node-resolve';
-import pkg from './package.json';
+import svelte from "rollup-plugin-svelte";
+import autoPreprocess from "svelte-preprocess";
+import scss from "rollup-plugin-scss";
+import resolve from "rollup-plugin-node-resolve";
+import pkg from "./package.json";
 
 const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
+  .replace(/^\w/, (m) => m.toUpperCase())
+  .replace(/-\w/g, (m) => m[1].toUpperCase());
 
 export default {
-	input: 'main.js',
-	output: [
-		{ file: 'build/' + pkg.module, format: 'es' },
-		{ file: 'build/' + pkg.main, format: 'umd', name }
-	],
-	plugins: [
-		svelte({
-			preprocess: autoPreprocess({ /* options */ })
-		}),
-		resolve()
-	]
+  input: "main.js",
+  output: [
+    { file: pkg.module, format: "es" },
+    { file: pkg.main, format: "umd", name },
+  ],
+  plugins: [
+    svelte({
+      preprocess: autoPreprocess({
+        /* options */
+      }),
+    }),
+    scss({
+      output: "build/index.css",
+    }),
+    resolve(),
+  ],
 };
